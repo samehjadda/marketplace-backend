585d1e8cba255c9e312413deba38c35290b9a845
samehjadda <samehjadda@gmail.com>
2023-05-29 13:46:27 -0400

    create, update, delete routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,18 @@
+import { createNewListingRoute } from "./createNewListing";
+import { deleteListingRoute } from "./deleteListing";
 import { getAllListingsRoute } from "./getAllListings";
-import { getListingRoute } from "./getListing";
-import { addViewToListingRoute
 
-} from "./addViewToListing";
+import { getListingRoute } from "./getListing";
+import { addViewToListingRoute } from "./addViewToListing";
+import { getUserListingsRoute } from "./getUserListings";
+import { updateListingRoute } from "./updateListing";
 export default [
     getAllListingsRoute,
+    createNewListingRoute,
     getListingRoute,
     addViewToListingRoute,
-    
+    getUserListingsRoute,
+    updateListingRoute,
+    deleteListingRoute,
+
 ]

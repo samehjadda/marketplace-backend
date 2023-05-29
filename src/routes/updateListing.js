5c7856832fe94331ed41d050fb3872f92636842c
samehjadda <samehjadda@gmail.com>
2023-05-29 18:05:26 -0400

    bug fixes

--- src/routes/updateListing.js
+++ src/routes/updateListing.js
@@ -4,16 +4,16 @@ export const updateListingRoute = {
     method: 'POST',
     path: '/api/listings/{id}',
     handler: async (req, h) => {
-        const { id }= req.params.id;
+        const { id }= req.params;
         const { name , description , price } = req.payload;
         const userId = '12345';
 
         await db.query(
             `UPDATE listings
-            SET name=?, description=?, price?
+            SET name=?, description=?, price=?
             WHERE id=? AND user_id=?`, 
             [name,description,price,id,userId]
-        );
+        );        
             const { results } = await db.query(
                 'SELECT * FROM listings WHERE id=? AND user_id=?'
                 ,[id,userId]

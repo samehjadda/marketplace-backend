afbcc6003300676e4632770c132a61f7b5aff080
samehjadda <samehjadda@gmail.com>
2023-05-30 16:28:22 -0400

    add auth

--- src/routes/createNewListing.js
+++ src/routes/createNewListing.js
@@ -1,14 +1,18 @@
 import { v4 as uuid} from "uuid";
+import * as admin from 'firebase-admin'
 import { db } from "../database";
 
 export const createNewListingRoute= {
     method: 'POST',
     path: '/api/listings',
     handler: async (req, h) => {
+
+        const token = req.headers.authtoken;
+        const user = await admin.auth().verifyIdToken(token);
+        const userId = user.user_id;
         
         const id = uuid();
         const { name , description , price } = req.payload;
-        const userId = '12345';
         const views = 0;
 
         await db.query(`
--- src/routes/deleteListing.js
+++ src/routes/deleteListing.js
@@ -1,13 +1,19 @@
 import { db } from "../database";
+import * as admin from 'firebase-admin'
 
 export const deleteListingRoute = {
     method: 'DELETE',
     path: '/api/listings/{id}',
     handler: async (req, h) => {
         const { id } = req.params;
+
+        const token = req.headers.authtoken;
+        const user = await admin.auth().verifyIdToken(token);
+        const userId = user.user_id;
+
         await db.query(
-            'DELETE FROM listings WHERE id=?',
-            [id],
+            'DELETE FROM listings WHERE id=? AND user_id=?',
+            [id, userId],
         );
         return { message: 'Success' }
     }
